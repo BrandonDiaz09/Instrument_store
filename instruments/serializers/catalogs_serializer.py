f1ce9ca66552c3f2ec7d18730c7ffe9fd7b79b32
brandondiaz <brandon.diaz.dev@gmail.com>
2025-06-21 15:03:01 -0600

    Ultima entrega

--- instruments/serializers/catalogs_serializer.py
+++ instruments/serializers/catalogs_serializer.py
@@ -25,3 +25,34 @@ class InstrumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Instrument
         fields = '__all__'
+
+class InstrumentTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Type
+        fields = ['id', 'name']
+
+class InstrumentBrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ['id', 'name']
+
+class InstrumentMaterialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Material
+        fields = ['id', 'name']
+
+class InstrumentUseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Use
+        fields = ['id', 'name']
+
+class InstrumentSerializer(serializers.ModelSerializer):
+    type = InstrumentTypeSerializer()
+    brand = InstrumentBrandSerializer()
+    material = InstrumentMaterialSerializer()
+    use = InstrumentUseSerializer()
+
+    class Meta:
+        model = Instrument
+        fields = ['id', 'name', 'price', 'creation_date', 'type', 'brand', 'material', 'use', 'description', 'url_imagen']
+
